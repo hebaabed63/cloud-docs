dda6a39dd4781ab1bf674aaf320e77000c91be0c
Heba Abed <abedheba63@gmail.com>
2025-06-09 16:04:06 +0300

    Fix secure asset path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document Cloud - @yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/main.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@700&family=Noto+Naskh+Arabic:wght@600&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -16,6 +16,6 @@
 
     @include('partials.footer')
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
 </body>
 </html>
